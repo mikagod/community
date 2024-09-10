caf995ef6d39b2df0cc5926bb055b8a287e06e93
mikas <2230959431@qq.com>
2024-09-10 21:30:12 +0800

    Initial commit

---
+++ src/main/java/com/fantasysky/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.fantasysky.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
